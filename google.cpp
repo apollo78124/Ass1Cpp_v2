2a740bdda1bd29cbf864f4a081a22d80b22cd629
apollo78124 <98apollo@naver.com>
2018-10-14 15:46:12 -0700

    google

---
+++ google.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <math.h>
+#include "matrix.hpp"
+
+using namespace std;
+
+class Google {
+
+public:
+    Google();
+
+    Google(int n);
+};
+
+
---
+++ google.hpp
@@ -0,0 +1,8 @@
+//
+// Created by dldms on 10/14/2018.
+//
+
+#ifndef ASS1CPP_V2_GOOGLE_HPP
+#define ASS1CPP_V2_GOOGLE_HPP
+
+#endif //ASS1CPP_V2_GOOGLE_HPP
